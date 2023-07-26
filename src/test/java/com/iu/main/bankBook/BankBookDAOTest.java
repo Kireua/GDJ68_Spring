c984fee528935c5734059713f158a68042c645fc
Kireua <akrhkdx@google.com>
2023-07-26 11:05:43 +0900

    C

--- src/test/java/com/iu/main/bankBook/BankBookDAOTest.java
+++ src/test/java/com/iu/main/bankBook/BankBookDAOTest.java
@@ -3,14 +3,27 @@ package com.iu.main.bankBook;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.iu.main.MyTest;
 
 public class BankBookDAOTest extends MyTest {
+	
+	@Autowired
+	private BankBookDAO bankBookDAO;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addTest() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		
+		for(int i=0; i<30; i++) {
+			bankBookDTO.setBookName("서브프라임모기지"+i);
+			bankBookDTO.setBookContents("서브프라임모기지론내용"+i);
+			bankBookDTO.setBookRate(9.8);
+			bankBookDTO.setBookSale(1);
+			bankBookDAO.setAdd(bankBookDTO);
+		}
+		System.out.println("finish");
 	}
 
 }
